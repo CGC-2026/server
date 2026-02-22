b6e4ab0497e9bbb0b3e6f4fcb8c79a8925287192
NicolaSavino <nick.savino@arcurve.com>
2026-02-22 14:14:14 -0700

    Added WithTransaction to store

--- internal/data/store.go
+++ internal/data/store.go
@@ -2,9 +2,11 @@ package data
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"time"
 
+	"server/internal/db"
 	sqlc "server/internal/db"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -35,3 +37,29 @@ func NewStore(ctx context.Context) (*Store, error) {
 }
 
 func (s *Store) Close() { s.Pool.Close() }
+
+// WithTransaction handles the boilerplate of starting, committing, and rolling back a transaction.
+func (s *Store) WithTransaction(ctx context.Context, fn func(*db.Queries) error) error {
+	tx, err := s.Pool.Begin(ctx)
+	if err != nil {
+		return fmt.Errorf("Failed to begin transaction: %w", err)
+	}
+
+	// Ensure rollback if not committed
+	defer tx.Rollback(ctx)
+
+	// Create a new Queries instance with the transaction
+	qtx := s.Queries.WithTx(tx)
+
+	// Execute the function
+	if err := fn(qtx); err != nil {
+		return err
+	}
+
+	// If no errror, commit the transaction
+	if err := tx.Commit(ctx); err != nil {
+		return fmt.Errorf("failed to commit transaction: %w", err)
+	}
+
+	return nil
+}
